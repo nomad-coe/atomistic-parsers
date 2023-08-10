a9ee28325ad3c0385784664c567e9745ac02d9f1
Alvin Noe Ladines <ladinesalvinnoe@gmail.com>
2023-08-10 11:39:59 +0200

    Remove msd and rdf calc in lammps parser

--- atomisticparsers/lammps/parser.py
+++ atomisticparsers/lammps/parser.py
@@ -927,66 +927,6 @@ class LammpsParser(MDParser):
             else:
                 workflow.method.thermodynamic_ensemble = 'NVE'
 
-            # calculate molecular radial distribution functions
-            sec_results = workflow.results
-            n_traj_split = 10  # number of intervals to split trajectory into for averaging
-            interval_indices = []  # 2D array specifying the groups of the n_traj_split intervals to be averaged
-            # first 20% of trajectory
-            interval_indices.append(np.arange(int(n_traj_split * 0.20)))
-            # last 80% of trajectory
-            interval_indices.append(np.arange(n_traj_split)[len(interval_indices[0]):])
-            # last 60% of trajectory
-            interval_indices.append(np.arange(n_traj_split)[len(interval_indices[0]) * 2:])
-            # last 40% of trajectory
-            interval_indices.append(np.arange(n_traj_split)[len(interval_indices[0]) * 3:])
-
-            # calculate molecular radial distribution functions
-            rdf_results = self.traj_parsers.eval('calc_molecular_rdf', n_traj_split=n_traj_split, n_prune=self.frame_rate, interval_indices=interval_indices)
-            if rdf_results is None:
-                rdf_results = self._mdanalysistraj_parser.calc_molecular_rdf(n_traj_split=n_traj_split, n_prune=self.frame_rate, interval_indices=interval_indices)
-            if rdf_results is not None and self.traj_parsers.eval('n_frames') is not None:
-                sec_rdfs = sec_results.m_create(RadialDistributionFunction)
-                sec_rdfs.type = 'molecular'
-                sec_rdfs.n_smooth = rdf_results.get('n_smooth')
-                sec_rdfs.n_prune = self.frame_rate
-                sec_rdfs.variables_name = np.array(['distance'])
-                for i_pair, pair_type in enumerate(rdf_results.get('types', [])):
-                    sec_rdf_values = sec_rdfs.m_create(RadialDistributionFunctionValues)
-                    sec_rdf_values.label = str(pair_type)
-                    sec_rdf_values.n_bins = len(rdf_results.get('bins', [[]] * i_pair)[i_pair])
-                    sec_rdf_values.bins = rdf_results['bins'][i_pair] if rdf_results.get(
-                        'bins') is not None else []
-                    sec_rdf_values.value = rdf_results['value'][i_pair] if rdf_results.get(
-                        'value') is not None else []
-                    sec_rdf_values.frame_start = rdf_results['frame_start'][i_pair] if rdf_results.get(
-                        'frame_start') is not None else []
-                    sec_rdf_values.frame_end = rdf_results['frame_end'][i_pair] if rdf_results.get(
-                        'frame_end') is not None else []
-
-            # calculate the molecular mean squared displacements
-            msd_results = self.traj_parsers.eval('calc_molecular_mean_squard_displacements')
-            msd_results = msd_results() if msd_results is not None else None
-            if msd_results is None:
-                msd_results = self._mdanalysistraj_parser.calc_molecular_mean_squared_displacements()
-            if msd_results is not None and self.traj_parsers.eval('n_frames') is not None:
-                sec_msds = sec_results.m_create(MeanSquaredDisplacement)
-                sec_msds.type = 'molecular'
-                sec_msds.direction = 'xyz'
-                for i_type, moltype in enumerate(msd_results.get('types', [])):
-                    sec_msd_values = sec_msds.m_create(MeanSquaredDisplacementValues)
-                    sec_msd_values.label = str(moltype)
-                    sec_msd_values.n_times = len(msd_results.get('times', [[]] * i_type)[i_type])
-                    sec_msd_values.times = msd_results['times'][i_type] if msd_results.get(
-                        'times') is not None else []
-                    sec_msd_values.value = msd_results['value'][i_type] if msd_results.get(
-                        'value') is not None else []
-                    sec_diffusion = sec_msd_values.m_create(DiffusionConstantValues)
-                    sec_diffusion.value = msd_results['diffusion_constant'][i_type] if msd_results.get(
-                        'diffusion_constant') is not None else []
-                    sec_diffusion.error_type = 'Pearson correlation coefficient'
-                    sec_diffusion.errors = msd_results['error_diffusion_constant'][i_type] if msd_results.get(
-                        'error_diffusion_constant') is not None else []
-
         self.archive.workflow2 = workflow
 
     def parse_system(self):

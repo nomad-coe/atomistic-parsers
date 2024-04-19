45d4091d2c4743238b327d6fba667a91a31dde74
Alvin Noe Ladines <ladinesalvinnoe@gmail.com>
2024-04-19 11:51:54 +0200

    Fix test

--- atomisticparsers/gromacs/parser.py
+++ atomisticparsers/gromacs/parser.py
@@ -1446,16 +1446,17 @@ class GromacsParser(MDParser):
 
             self.parse_md_workflow(dict(method=method, results=results))
 
-            if flag_fe and self.archive.m_context:
+            if flag_fe:
                 sec_fe_parameters = (
                     self.archive.workflow2.method.free_energy_calculation_parameters[0]
                 )
                 sec_fe = self.archive.workflow2.results.free_energy_calculations[0]
                 sec_fe.method_ref = sec_fe_parameters
-                sec_fe.value_total_energy_magnitude = columns[:, 0]
-                sec_fe.value_total_energy_derivative_magnitude = columns[:, 1]
-                sec_fe.value_total_energy_differences_magnitude = columns[:, 2:-1]
-                sec_fe.value_PV_energy_magnitude = columns[:, -1]
+                if self.archive.m_context:
+                    sec_fe.value_total_energy_magnitude = columns[:, 0]
+                    sec_fe.value_total_energy_derivative_magnitude = columns[:, 1]
+                    sec_fe.value_total_energy_differences_magnitude = columns[:, 2:-1]
+                    sec_fe.value_PV_energy_magnitude = columns[:, -1]
 
     def parse_input(self):
         sec_run = self.archive.run[-1]

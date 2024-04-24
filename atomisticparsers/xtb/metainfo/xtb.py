7af4f7cc9acbbba749f28e0869ef120fca978f3f
Alvin Noe Ladines <ladinesalvinnoe@gmail.com>
2024-04-24 11:40:19 +0200

    Fix xtb

--- atomisticparsers/xtb/metainfo/xtb.py
+++ atomisticparsers/xtb/metainfo/xtb.py
@@ -16,10 +16,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-import numpy as np            # pylint: disable=unused-import
+import numpy as np  # pylint: disable=unused-import
 
 from nomad.metainfo import (  # pylint: disable=unused-import
-    Package, Quantity, Section, SubSection, JSON
+    Package,
+    Quantity,
+    Section,
+    SubSection,
+    JSON,
 )
 import runschema.run  # pylint: disable=unused-import
 import runschema.calculation  # pylint: disable=unused-import
@@ -32,195 +36,221 @@ m_package = Package()
 
 
 class Run(runschema.method.Method):
-
     m_def = Section(validate=False, extends_base_section=True)
 
     x_xtb_calculation_setup = Quantity(
         type=JSON,
         shape=[],
-        description='''
-        ''')
+        description="""
+        """,
+    )
 
 
 class TB(runschema.method.TB):
-
     m_def = Section(validate=False, extends_base_section=True)
 
     x_xtb_setup = Quantity(
         type=JSON,
         shape=[],
-        description='''
-        ''')
+        description="""
+        """,
+    )
 
 
 class Energy(runschema.calculation.Energy):
-
     m_def = Section(validate=False, extends_base_section=True)
 
     x_xtb_scc = SubSection(sub_section=runschema.calculation.EnergyEntry.m_def)
 
     x_xtb_isotropic_es = SubSection(sub_section=runschema.calculation.EnergyEntry.m_def)
 
-    x_xtb_anisotropic_es = SubSection(sub_section=runschema.calculation.EnergyEntry.m_def)
+    x_xtb_anisotropic_es = SubSection(
+        sub_section=runschema.calculation.EnergyEntry.m_def
+    )
 
-    x_xtb_anistropic_xc = SubSection(sub_section=runschema.calculation.EnergyEntry.m_def)
+    x_xtb_anistropic_xc = SubSection(
+        sub_section=runschema.calculation.EnergyEntry.m_def
+    )
 
     x_xtb_dispersion = SubSection(sub_section=runschema.calculation.EnergyEntry.m_def)
 
     x_xtb_repulsion = SubSection(sub_section=runschema.calculation.EnergyEntry.m_def)
 
-    x_xtb_halogen_bond_corr = SubSection(sub_section=runschema.calculation.EnergyEntry.m_def)
+    x_xtb_halogen_bond_corr = SubSection(
+        sub_section=runschema.calculation.EnergyEntry.m_def
+    )
 
-    x_xtb_add_restraining = SubSection(sub_section=runschema.calculation.EnergyEntry.m_def)
+    x_xtb_add_restraining = SubSection(
+        sub_section=runschema.calculation.EnergyEntry.m_def
+    )
 
 
 class MultipolesEntry(runschema.calculation.MultipolesEntry):
-
     m_def = Section(validate=False, extends_base_section=True)
 
     x_xtb_q_only = Quantity(
-        type=np.dtype(np.float64),
-        shape=['n_multipoles'],
-        description='''
-        ''')
+        type=np.float64,
+        shape=["n_multipoles"],
+        description="""
+        """,
+    )
 
     x_xtb_q_plus_dip = Quantity(
-        type=np.dtype(np.float64),
-        shape=['n_multipoles'],
-        description='''
-        ''')
+        type=np.float64,
+        shape=["n_multipoles"],
+        description="""
+        """,
+    )
 
 
 class GeometryOptimization(simulationworkflowschema.GeometryOptimization):
-
     m_def = Section(validate=False, extends_base_section=True)
 
     x_xtb_optimization_level = Quantity(
         type=str,
         shape=[],
-        description='''
-        ''')
+        description="""
+        """,
+    )
 
     x_xtb_max_opt_cycles = Quantity(
-        type=np.dtype(np.int32),
+        type=np.int32,
         shape=[],
-        description='''
-        ''')
+        description="""
+        """,
+    )
 
     x_xtb_anc_micro_cycles = Quantity(
-        type=np.dtype(np.int32),
+        type=np.int32,
         shape=[],
-        description='''
-        ''')
+        description="""
+        """,
+    )
 
     x_xtb_n_degrees_freedom = Quantity(
-        type=np.dtype(np.int32),
+        type=np.int32,
         shape=[],
-        description='''
-        ''')
+        description="""
+        """,
+    )
 
     x_xtb_rf_solver = Quantity(
         type=str,
         shape=[],
-        description='''
-        ''')
+        description="""
+        """,
+    )
 
     x_xtb_linear = Quantity(
         type=str,
         shape=[],
-        description='''
-        ''')
+        description="""
+        """,
+    )
 
     x_xtb_hlow = Quantity(
-        type=np.dtype(np.float64),
+        type=np.float64,
         shape=[],
-        description='''
-        ''')
+        description="""
+        """,
+    )
 
     x_xtb_hmax = Quantity(
-        type=np.dtype(np.float64),
+        type=np.float64,
         shape=[],
-        description='''
-        ''')
+        description="""
+        """,
+    )
 
     x_xtb_s6 = Quantity(
-        type=np.dtype(np.float64),
+        type=np.float64,
         shape=[],
-        description='''
-        ''')
+        description="""
+        """,
+    )
 
 
 class MolecularDynamics(simulationworkflowschema.MolecularDynamics):
-
     m_def = Section(validate=False, extends_base_section=True)
 
     x_xtb_md_time = Quantity(
-        type=np.dtype(np.float64),
+        type=np.float64,
         shape=[],
-        unit='s',
-        description='''
-        ''')
+        unit="s",
+        description="""
+        """,
+    )
 
     x_xtb_scc_accuracy = Quantity(
-        type=np.dtype(np.float64),
+        type=np.float64,
         shape=[],
-        description='''
-        ''')
+        description="""
+        """,
+    )
 
     x_xtb_temperature = Quantity(
-        type=np.dtype(np.float64),
+        type=np.float64,
         shape=[],
-        unit='K',
-        description='''
-        ''')
+        unit="K",
+        description="""
+        """,
+    )
 
     x_xtb_max_steps = Quantity(
-        type=np.dtype(np.int32),
+        type=np.int32,
         shape=[],
-        description='''
-        ''')
+        description="""
+        """,
+    )
 
     x_xtb_max_block_length = Quantity(
-        type=np.dtype(np.int32),
+        type=np.int32,
         shape=[],
-        description='''
-        ''')
+        description="""
+        """,
+    )
 
     x_xtb_dumpstep_trj = Quantity(
-        type=np.dtype(np.float64),
+        type=np.float64,
         shape=[],
-        unit='s',
-        description='''
-        ''')
+        unit="s",
+        description="""
+        """,
+    )
 
     x_xtb_dumpstep_coords = Quantity(
-        type=np.dtype(np.float64),
+        type=np.float64,
         shape=[],
-        unit='s',
-        description='''
-        ''')
+        unit="s",
+        description="""
+        """,
+    )
 
     x_xtb_h_atoms_mass = Quantity(
-        type=np.dtype(np.float64),
+        type=np.float64,
         shape=[],
-        unit='kg',
-        description='''
-        ''')
+        unit="kg",
+        description="""
+        """,
+    )
 
     x_xtb_n_degrees_freedom = Quantity(
-        type=np.dtype(np.int32),
+        type=np.int32,
         shape=[],
-        description='''
-        ''')
+        description="""
+        """,
+    )
 
     x_xtb_shake_bonds = Quantity(
-        type=np.dtype(np.int32),
+        type=np.int32,
         shape=[],
-        description='''
-        ''')
+        description="""
+        """,
+    )
 
     x_xtb_berendsen = Quantity(
         type=bool,
         shape=[],
-        description='''
-        ''')
+        description="""
+        """,
+    )

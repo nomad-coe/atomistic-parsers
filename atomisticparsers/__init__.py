e9b52a29bdb76c02117337e4a914235f540c4755
Alvin Noe Ladines <ladinesalvinnoe@gmail.com>
2024-08-22 13:21:45 +0200

    Add entry point

--- atomisticparsers/__init__.py
+++ atomisticparsers/__init__.py
@@ -16,17 +16,113 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+from nomad.config.models.plugins import ParserEntryPoint
 
-# from .amber.parser import AmberParser
-# from .asap.parser import AsapParser
-# from .bopfox.parser import BOPfoxParser
-# from .dftbplus.parser import DFTBPlusParser
-# from .dlpoly.parser import DLPolyParser
-# from .gromacs.parser import GromacsParser
-# from .gromos.parser import GromosParser
-# from .gulp.parser import GulpParser
-# from .lammps.parser import LammpsParser
-# from .libatoms.parser import LibAtomsParser
-# from .namd.parser import NAMDParser
-# from .tinker.parser import TinkerParser
-# from .xtb.parser import XTBParser
+
+class EntryPoint(ParserEntryPoint):
+    def load(self):
+        from nomad.parsing import MatchingParserInterface
+        from . import (
+            amber,
+            asap,
+            bopfox,
+            dftbplus,
+            dlpoly,
+            gromacs,
+            gromos,
+            gulp,
+            h5md,
+            lammps,
+            libatoms,
+            namd,
+            tinker,
+            xtb,
+            utils,
+        )
+
+        return MatchingParserInterface(self.parser_class_name)
+
+
+amber_parser_entry_point = EntryPoint(
+    name='parsers/amber',
+    description='NOMAD parser for AMBER.',
+    parser_class_name='atomisticparsers.amber.AmberParser',
+)
+
+asap_parser_entry_point = EntryPoint(
+    name='parsers/asap',
+    description='NOMAD parser for ASAP.',
+    parser_class_name='atomisticparsers.asap.AsapParser',
+)
+
+bopfox_parser_entry_point = EntryPoint(
+    name='parsers/bopfox',
+    description='NOMAD parser for BOPFOX.',
+    parser_class_name='atomisticparsers.bopfox.BOPfoxParser',
+)
+
+dftbplus_parser_entry_point = EntryPoint(
+    name='parsers/dftbplus',
+    description='NOMAD parser for DFTBPLUS.',
+    parser_class_name='atomisticparsers.dftbplus.DFTBPlusParser',
+)
+
+dlpoly_parser_entry_point = EntryPoint(
+    name='parsers/dlpoly',
+    description='NOMAD parser for DLPOLY.',
+    parser_class_name='atomisticparsers.dlpoly.DLPolyParser',
+)
+
+gromacs_parser_entry_point = EntryPoint(
+    name='parsers/gromacs',
+    description='NOMAD parser for GROMACS.',
+    parser_class_name='atomisticparsers.gromacs.GromacsParser',
+)
+
+gromos_parser_entry_point = EntryPoint(
+    name='parsers/gromos',
+    description='NOMAD parser for GROMOS.',
+    parser_class_name='atomisticparsers.gromos.GromosParser',
+)
+
+gulp_parser_entry_point = EntryPoint(
+    name='parsers/gulp',
+    description='NOMAD parser for GULP.',
+    parser_class_name='atomisticparsers.gulp.GulpParser',
+)
+
+h5md_parser_entry_point = EntryPoint(
+    name='parsers/h5md',
+    description='NOMAD parser for H5MD.',
+    parser_class_name='atomisticparsers.h5md.H5MDParser',
+)
+
+lammps_parser_entry_point = EntryPoint(
+    name='parsers/lammps',
+    description='NOMAD parser for LAMMPS.',
+    parser_class_name='atomisticparsers.lammps.LammpsParser',
+)
+
+libatoms_parser_entry_point = EntryPoint(
+    name='parsers/libatoms',
+    description='NOMAD parser for LIBATOMS.',
+    parser_class_name='atomisticparsers.libatoms.LibAtomsParser',
+)
+
+namd_parser_entry_point = EntryPoint(
+    name='parsers/namd',
+    description='NOMAD parser for NAMD.',
+    parser_class_name='atomisticparsers.namd.NAMDParser',
+)
+
+tinker_parser_entry_point = EntryPoint(
+    name='parsers/tinker',
+    description='NOMAD parser for TINKER.',
+    parser_class_name='atomisticparsers.tinker.TinkerParser',
+)
+
+xtb_parser_entry_point = EntryPoint(
+    name='parsers/xtb',
+    description='NOMAD parser for XTB.',
+    parser_class_name='atomisticparsers.xtb.XTBParser',
+)

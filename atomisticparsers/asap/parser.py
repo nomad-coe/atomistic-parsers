99b0ac613560e4d89fd1476d49e2040a07cf29b3
Julia Schumann <jul.schumann86@gmail.com>
2024-09-06 16:06:02 +0200

    initial commit for trajectory parser

--- atomisticparsers/asap/parser.py
+++ atomisticparsers/asap/parser.py
@@ -42,6 +42,9 @@ class TrajParser(FileParser):
                 self._file_handler = Trajectory(self.mainfile, 'r')
                 # check if traj file is really asap
                 if 'calculator' in self._file_handler.backend.keys():
+                    if self._file_handler.backend.calculator.name == 'VASP':
+                        self.logger.error('Trajectory is from a VASP calculation.')
+                        self._file_handler = None
                     if self._file_handler.backend.calculator.name != 'emt':  # pylint: disable=E1101
                         self.logger.error('Trajectory is not ASAP.')
                         self._file_handler = None
